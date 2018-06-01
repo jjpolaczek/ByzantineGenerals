926fc9a4dffc6b9b010cc39ccc39954da66c439d
Jakub Polaczek <jj.polaczek@gmail.com>
2018-06-01 13:03:23 +0200

    Added additional test logging for report output

--- testSolution.py
+++ testSolution.py
@@ -5,6 +5,7 @@ import time
 import signal
 import random
 import logging
+import os
 logger = logging.getLogger(__name__)
 #msg = Message()
 #print "Hello"
@@ -47,11 +48,37 @@ def testComms(no_generals):
 
 class TestSolution():
     def __init__(self, basePort=37000):
+        self.basePort = basePort
+        self._resetTestVariables()
+        #The result logger will log result counts for keys in tuple form: (no_generals, no_traitors) and increment fail counters
+        self.resetFails()
+
+
+    def _resetTestVariables(self):
         self.generals = []
         self.traitors= []
         self.problemStructure = {}
         self.commanderNum = None
-        self.basePort = basePort
+
+    def resetFails(self):
+        self.resultFailLogger = {}
+
+    def _updateFails(self, result):
+        no_generals = len(self.generals)
+        no_traitors = len(self.traitors)
+        #Checks IC1 and IC2 conditions
+        if (result[0] is not True) or (result[1] not in [None, True]):
+            try:
+                self.resultFailLogger[(no_generals, no_traitors)] += 1
+            except KeyError:
+                self.resultFailLogger[(no_generals, no_traitors)] = 1
+    def saveFailsCSV(self, filename):
+        with open(filename, 'w') as f:
+            f.write("sep=;" + os.linesep)
+            f.write("Generals;Traitors;Fails" + os.linesep)
+            for key in self.resultFailLogger:
+                f.write("%d;%d;%d" %(key[0], key[1], self.resultFailLogger[key]) + os.linesep)
+
 
     def _setup(self, no_generals, no_traitors, latencyAvgms=0, latencyDiffms=0, traitorFailRate=0.0, globalFailRate=0.0):
         # Create traitors listing:
@@ -87,7 +114,7 @@ class TestSolution():
         for gen in self.generals:
             gen.join()
         #reset state
-        self.__init__(self.basePort)
+        self._resetTestVariables()
     def _converge(self, timeout):
         timeoutMax = time.time() + timeout
 
@@ -134,10 +161,13 @@ class TestSolution():
             logger.warn("Failed Conditions for %d generals and %d traitors", len(self.generals), len(self.traitors))
             logger.warn("Commander: %s", self.generals[self.commanderNum].name)
             logger.warn("Traitors: %s", self.traitors)
-            for gen in self.generals:
-                logger.info("%s state: %s, decision %s", gen.name, gen.getState(), gen._decision)
-                gen.debugStateTree()
-                time.sleep(0.5)
+            max_traitors = int((len(self.generals) - 1) / 3.0)
+            #Print only if the assumption of traitor count is not fullfilled
+            if len(self.traitors) <= max_traitors:
+                for gen in self.generals:
+                    logger.info("%s state: %s, decision %s", gen.name, gen.getState(), gen._decision)
+                    gen.debugStateTree()
+                    time.sleep(0.5)
     def byzantineTest(self, no_generals, no_traitors):
         result=None
         tstart = time.time()
@@ -159,6 +189,7 @@ class TestSolution():
             result = self._verify()
             logger.info("IC1: %s, IC2: %s" % result)
             self._printIfFail(result)
+            self._updateFails(result)
             if False:
                 for gen in self.generals:
                     logger.info("%s state: %s, decision %s", gen.name, gen.getState(), gen._decision)
@@ -200,6 +231,7 @@ class TestSolution():
             result = self._verify()
             logger.info("IC1: %s, IC2: %s" % result)
             self._printIfFail(result)
+            self._updateFails(result)
 
             self._cleanup()
         except ServiceExit:
@@ -219,7 +251,12 @@ class TestSolution():
             self._setup(no_generals, no_traitors, latencyAvgms=(latencyMax + latencyMin) / 2.0, latencyDiffms=latencyMax - latencyMin)
             self._start()
             # Pick a random general ro pass the decision to the network
-            selection = random.choice(range(no_generals))
+            selection = 0
+            for gen in self.generals:
+                if gen.name not in self.traitors:
+                    break
+                else:
+                    selection += 1
             logger.debug("Selected %s to be a commanding general" % self.generals[selection].name)
             self.commanderNum = selection
             # Pick random decision to be made
@@ -232,6 +269,7 @@ class TestSolution():
             result = self._verify()
             logger.info("IC1: %s, IC2: %s" % result)
             self._printIfFail(result)
+            self._updateFails(result)
 
             self._cleanup()
         except ServiceExit:
@@ -266,6 +304,7 @@ class TestSolution():
             result = self._verify()
             logger.info("IC1: %s, IC2: %s" % result)
             self._printIfFail(result)
+            self._updateFails(result)
 
             self._cleanup()
         except ServiceExit:
@@ -279,26 +318,27 @@ class TestSolution():
 
 def main():
     # Register the signal handlers
-    logging.basicConfig(level=logging.WARN)
+    logging.basicConfig(level=logging.ERROR)
     logging.getLogger('__main__').setLevel(logging.INFO)
     signal.signal(signal.SIGTERM, service_shutdown)
     signal.signal(signal.SIGINT, service_shutdown)
     #Create test object and run tests
     tester = TestSolution()
-    generalCountTested = range(3, 8)
-    retries = 2
-
+    generalCountTested = range(3, 11)
+    retries = 100
     #Basic testing loop for traitorous and faithful generals
     for gen_count in generalCountTested:
         #3m +1 generals cope with m traitors so
-        # for k generals we can have (k - 1) / 3 traitors rounded down
-        max_traitors = int((gen_count - 1) / 3.0)
+        # for k generals we can have (k - 1) / 3 traitors rounded down, we test for more than minimum no of traitors!!!
+        max_traitors = int((gen_count - 1) / 3.0) + 1
         for traitors in range(0, max_traitors + 1):
             for retry in range(retries):
                 tester.byzantineTest(gen_count, traitors)
-
+    tester.saveFailsCSV("byzantine_standard%d.csv" % retries)
+    tester.resetFails()
+    return
     # Testing loop for traitorous commanding generals
-    retries = 2
+    retries = 0
     generalCountTested = range(4, 8)
     for gen_count in generalCountTested:
         #3m +1 generals cope with m traitors so
@@ -309,7 +349,7 @@ def main():
                 tester.byzantineTestCommanderTraitor(gen_count, traitors)
 
     # Testing loop for traitorous and faithful generals with latency
-    retries = 2
+    retries = 0
     generalCountTested = range(4, 8)
     latencyMin_ms = 100
     latencyMax_ms = 300
@@ -324,11 +364,11 @@ def main():
 
     # Testing loop for traitorous and faithful generals  with latency and failures
     retries = 2
-    generalCountTested = range(4, 8)
+    generalCountTested = range(6, 8)
     latencyMin_ms = 100
     latencyMax_ms = 300
     traitorFailRates = [0.2, 0.8, 1.0]
-    globalFailRates = [0.0, 0.05]
+    globalFailRates = [0.0, 0.00]
 
     for gen_count in generalCountTested:
         #3m +1 generals cope with m traitors so
